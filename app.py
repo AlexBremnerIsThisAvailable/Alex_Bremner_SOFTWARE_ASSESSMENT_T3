3bd6c1c671f8e340d55d6c95eaa893cc4ea26a00
Your Name <alexander.bremner1@education.nsw.gov.au>
2025-12-15 22:02:03 +1100

    fixed the login so it can send user to homepage and deleted the forgot password as it was non functional and not useful

--- app.py
+++ app.py
@@ -1,13 +1,72 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 import os
 
 app = Flask(__name__)
-app.secret_key = os.urandom(24)
+
+app.secret_key = os.urandom(24) 
+
+DATABASE_FILE = 'LoginData.db' 
 
 @app.route('/')
 def login():
     return render_template('login.html')
+
+@app.route('/login_validation', methods=['POST'])
+def login_validation():
+    Username = request.form.get('username') 
+    password = request.form.get('password')
+  
+    connection = sqlite3.connect(DATABASE_FILE) 
+    cursor = connection.cursor()
+  
+
+    user_records = cursor.execute("SELECT * FROM USERS WHERE Username = ? AND password = ?", (Username, password)).fetchall()
+    connection.close()
+
+    if len(user_records) > 0:
+
+        user_data = user_records[0]
+
+        return redirect(f'/home?username={user_data[0]}&password={user_data[1]}')
+    else: 
+        return redirect(url_for('login'))
+
+@app.route('/home')
+def home():
+
+    username = request.args.get('username')
+    password = request.args.get('password')
+    
+
+    return render_template('home.html', username=username, password=password)
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html') 
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    Username = request.form.get('username')
+    password = request.form.get('password')
+    
+    connection = sqlite3.connect(DATABASE_FILE)
+    cursor = connection.cursor()
+
+    try:
+
+        cursor.execute("INSERT INTO USERS (Username, password) VALUES (?, ?)", (Username, password))
+        connection.commit()
+        
+
+        return redirect(f'/home?username={Username}&password={password}')
+
+    except sqlite3.IntegrityError:
+        connection.rollback()
+        return redirect(url_for('signup'))
+        
+    finally:
+        connection.close()
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 import sqlite3
 
-connection = sqlite3.Connection('LoginData.db')
+connection = sqlite3.connect('LoginData.db')
 cursor = connection.cursor()
+
 
 cmd1 = """ CREATE TABLE IF NOT EXISTS USERS (Username varchar(50) primary key,
                                         password varchar(50) not null) """
@@ -11,10 +12,17 @@ cursor.execute(cmd1)
 
 cmd2 = """ INSERT INTO USERS (Username, password) values ('tester', 'tester1') """
 
-cursor.execute(cmd2)
-connection.commit()
+try:
+    cursor.execute(cmd2)
+    connection.commit()
+    print("Default user 'tester' created with a plain text password.")
+except sqlite3.IntegrityError:
+    print("User 'tester' already exists. Rolling back commit.")
+    connection.rollback()
 
 ans = cursor.execute("select * from USERS").fetchall()
-
+print("\nCurrent database users:")
 for i in ans:
-    print(i) 
+    print(i)
+
+connection.close()
