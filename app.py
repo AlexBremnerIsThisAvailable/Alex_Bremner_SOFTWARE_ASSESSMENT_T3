82a6e225fbd8fac4838185202f45ad496584766b
Your Name <alexander.bremner1@education.nsw.gov.au>
2025-12-16 22:30:28 +1100

    final_final changes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, abort
 import sqlite3
 import os
+from waitress import serve
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24) 
@@ -147,4 +148,4 @@ def admin_delete_account():
     return redirect(f'/adminhome?user={user}&role={role}')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    serve(app, host="0.0.0.0", port=8000)
